7f97c32715cface267ef60555480ee8bba334142
AWielgus <adrianwielgus99@gmail.com>
2022-01-15 21:25:57 +0100

    why its not working

--- djangoWebApp/urls.py
+++ djangoWebApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views
 
 urlpatterns = [
+    # path('abc/', views.home_view()),
     path('admin/', admin.site.urls),
 ]
--- products/models.py
+++ products/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-
 # Create your models here.
 class Product(models.Model):
-    name = models.TextField()
-    description = models.TextField()
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     amount = models.IntegerField(default=0)
-    price = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
     creation_date = models.DateTimeField('creation date')
     modification_date = models.DateTimeField('modification date')
+    hidden = models.BooleanField(default=False)
+
